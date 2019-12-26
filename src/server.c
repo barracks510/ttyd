adfd1b0805a41776afb6dc5785a974166a851ca7
Dennis Chen <barracks510@gmail.com>
2019-12-26 15:15:53 -0600

    server: handle port 0
    Currently, if a user selects port 0 (as advertised in the help menu), there is
    no way for the user to trivially find ttyd's port. This commit resolves the
    issue by logging the port to the console and by opening a URL with the correct
    port information if a user has selected such an option.

--- src/server.c
+++ src/server.c
@@ -483,9 +483,18 @@ main(int argc, char **argv) {
         return 1;
     }
 
+    struct lws_vhost *vhost = lws_get_vhost_by_name(context, "default");
+    if (vhost == NULL) {
+        lwsl_err("libwebsockets default vhost creation failed\n");
+        return 1;
+    }
+
+    int port = lws_get_vhost_listen_port(vhost);
+    lwsl_notice("  listening on port: %d\n", port);
+
     if (browser) {
         char url[30];
-        sprintf(url, "%s://localhost:%d", ssl ? "https" : "http", info.port);
+        sprintf(url, "%s://localhost:%d", ssl ? "https" : "http", port);
         open_uri(url);
     }
 
